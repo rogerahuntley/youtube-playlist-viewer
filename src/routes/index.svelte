398d4357c785313e61cb5ebf9ae6ea6d6b248c34
Roger Huntley <rogerh@hesonline.com>
2022-06-30 08:10:37 -0400

    improved css, added backgrounds

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,19 +13,23 @@
 
 <script>
 	export let videos = [];
-	console.log(videos);
 </script>
 
+<div id="background">
+	<iframe
+		src="https://www.youtube.com/embed/0jxUZxQBvb8?autoplay=1&mute=1&loop=1"
+		title="YouTube video player"
+	/>
+</div>
 <div id="main">
 	<ul id="circle">
-		{#each videos as video}
-			<li>
+		{#each videos as video, i}
+			<li style="--total-count: {videos.length}; --current:{i}">
 				<iframe
 					src="https://www.youtube.com/embed/{video}"
 					title="YouTube video player"
 					frameborder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
 				/>
 			</li>
 		{/each}
@@ -34,6 +38,24 @@
 
 <style lang="scss">
 	@use 'sass:math';
+
+	:global(body) {
+		padding: 0;
+		margin: 0;
+		overflow: hidden;
+	}
+
+	#background {
+		position: fixed;
+		width: 120%;
+		height: 120%;
+		left: -10%;
+		top: -10%;
+		iframe {
+			width: 100%;
+			height: 100%;
+		}
+	}
 
 	#main {
 		width: 100vw;
@@ -46,61 +68,62 @@
 	#circle {
 		position: relative;
 		background-color: red;
-	}
+		li {
+			display: grid;
+			place-content: center;
+			position: fixed;
+			width: 0px;
+			height: 0px;
 
-	#circle li {
-		display: grid;
-		place-content: center;
-		position: fixed;
-		width: 0px;
-		height: 0px;
+			iframe {
+				// original dimensions: 560 x 315
+				width: 40vmin;
+				height: 22.5vmin;
+			}
 
-		iframe {
-			// original dimensions: 560 x 315
-			width: 40vmin;
-			height: 22.5vmin;
-		}
+			@property --offset {
+				syntax: '<angle>';
+				inherits: true;
+				initial-value: 0deg;
+			}
 
-		@property --offset {
-			syntax: '<angle>';
-			inherits: true;
-			initial-value: 0deg;
-		}
+			$min: 1;
+			$max: 18; // max number to generate angles for (kind of hacky)
+			$speed: 10; // in seconds
 
-		$min: 1;
-		$max: 18; // max number to generate angles for (kind of hacky)
-		$speed: 10; // in seconds
+			$radius: 30vmin;
 
-		$radius: 40vmin;
-		//--offset: 0; // css variable to interpolate with animations
+			//--offset: 0; // css variable to interpolate with animations
+			--offset: calc(360deg / var(--total-count) * var(--current));
 
-		// finds number of total elements
-		@for $i from $min through $max {
-			&:first-child:nth-last-child(#{$i}),
-			&:first-child:nth-last-child(#{$i}) ~ li {
-				// finds current number in group of elements
-				@for $x from 1 through $i {
-					&:nth-child(#{$x}) {
-						// spin animation
-						@keyframes example-#{$i}-#{$x} {
-							0% {
-								// set position based on circle
-								$angle: $x * math.div(360, $i) + 0deg;
-								// rotate and push to radius
-								transform: rotate(#{$angle}) translate(#{$radius}) rotate(calc(-1 * #{$angle}));
-							}
-							100% {
-								$angle: $x * math.div(360, $i) + 360deg;
-								transform: rotate(#{$angle}) translate(#{$radius}) rotate(calc(-1 * #{$angle}));
-							}
-						}
+			// set position based on circle
+			--angle: calc(var(--offset) + 0deg);
+			// set position based on circle
+			--angle2: calc(var(--offset) + 360deg);
 
-						animation-name: example-#{$i}-#{$x};
-						animation-duration: #{$speed}s;
-						animation-iteration-count: infinite;
-						animation-timing-function: linear;
-					}
+			// spin animation
+			@keyframes spin {
+				0% {
+					// rotate and push to radius
+					transform: rotate(var(--angle)) translate(#{$radius}) rotate(calc(-1 * var(--angle)));
 				}
+				100% {
+					// rotate and push to radius
+					transform: rotate(var(--angle2)) translate(#{$radius}) rotate(calc(-1 * var(--angle2)));
+				}
+			}
+
+			animation-name: spin;
+			animation-duration: #{$speed}s;
+			animation-iteration-count: infinite;
+			animation-timing-function: linear;
+
+			&:nth-child(even) {
+				animation-direction: reverse;
+			}
+
+			&:hover {
+				animation-play-state: paused;
 			}
 		}
 	}
